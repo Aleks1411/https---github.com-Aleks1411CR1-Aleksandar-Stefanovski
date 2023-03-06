7bb11d0cc1dcd1618aac502f5e847b112e1e5985
Aleks1411 <addastefanovski@gmail.com>
2023-03-06 15:32:30 +0100

    ttt

--- indexjs.js
+++ indexjs.js
@@ -1,32 +1,28 @@
-(async () => {
+// (async () => {
 
-    const { value: email } = await Swal.fire({
-      title: 'Input email address',
-      input: 'email',
-      inputLabel: 'Your email address',
-      inputPlaceholder: 'Enter your email address'
-    })
+//     const { value: email } = await Swal.fire({
+//       title: 'Input email address',
+//       input: 'email',
+//       inputLabel: 'Your email address',
+//       inputPlaceholder: 'Enter your email address'
+//     })
     
-    if (email) {
-        const { value: password } = await Swal.fire({
-            title: 'Enter your password',
-            input: 'password',
-            inputLabel: 'Password',
-            inputPlaceholder: 'Enter your password',
-            inputAttributes: {
-              maxlength: 10,
-              autocapitalize: 'off',
-              autocorrect: 'off'
-            }
-          })
+//     if (email) {
+//         const { value: password } = await Swal.fire({
+//             title: 'Enter your password',
+//             input: 'password',
+//             inputLabel: 'Password',
+//             inputPlaceholder: 'Enter your password',
+//             inputAttributes: {
+//               maxlength: 10,
+//               autocapitalize: 'off',
+//               autocorrect: 'off'
+//             }
+//           })
           
-          if (password) {
-            Swal.fire(`Entered password: ${password}`)
-            console.log("hello");
-          }
-    }
-    
-    })()
-    
-    
-
+//           if (password) {
+//             Swal.fire(`Entered password: ${password}`)
+//             console.log("hello");
+//           }
+//     }
+//     })()
